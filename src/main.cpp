41b6095b5e140047302bebd31b307909625dcb8c
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-06-07 18:13:08 +0200

    printf

--- src/main.cpp
+++ src/main.cpp
@@ -4110,7 +4110,7 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv)
             if (fShutdown)
                 return true;
             if (fDebugNet || (vInv.size() == 1))
-                printf("received getdata for: %s\n", inv.ToString().c_str());
+//                printf("received getdata for: %s\n", inv.ToString().c_str());
 
             if (inv.type == MSG_BLOCK)
             {
