fd1be15902995853ebb4b3505241e61cf559cdcf
jondow72 <77569928+jondow72@users.noreply.github.com>
2025-07-24 09:35:23 +0200

    UPNP_GetValidIGD
    src/net.cpp: In function ‘void ThreadMapPort2(void*)’:
    src/net.cpp:1056:25: error: too few arguments to function ‘int UPNP_GetValidIGD(UPNPDev*, UPNPUrls*, IGDdatas*, char*, int, char*, int)’
     1056 |     r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
          |         ~~~~~~~~~~~~~~~~^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~

--- src/net.cpp
+++ src/net.cpp
@@ -1053,7 +1053,12 @@ void ThreadMapPort2(void* parg)
     struct IGDdatas data;
     int r;
 
+    #if defined(MINIUPNPC_API_VERSION) && MINIUPNPC_API_VERSION >= 14
+    char devUuid[64];
+    r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr), devUuid, sizeof(devUuid));
+    #else
     r = UPNP_GetValidIGD(devlist, &urls, &data, lanaddr, sizeof(lanaddr));
+    #endif
     if (r == 1)
     {
         if (fDiscover) {
