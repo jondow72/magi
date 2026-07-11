8e9b3f170c90eb981a3f05641d4a2b7c1a12c18f
jondow72 <77569928+jondow72@users.noreply.github.com>
2026-07-11 02:05:03 +0200

    Update main.h

--- src/main.h
+++ src/main.h
@@ -221,7 +221,7 @@ void IncrementExtraNonce(CBlock* pblock, CBlockIndex* pindexPrev, unsigned int& 
 void FormatHashBuffers(CBlock* pblock, char* pmidstate, char* pdata, char* phash1);
 bool CheckWork(CBlock* pblock, CWallet& wallet, CReserveKey& reservekey);
 bool CheckProofOfWork(uint256 hash, unsigned int nBits);
-int64 GetProofOfWorkReward(int nBits, int nHeight, int64 nFees);
+int64 GetProofOfWorkReward(int nBits, unsigned int nTime, int64 nFees);
 int64 GetProofOfWorkRewardV2(const CBlockIndex* pindexPrev, int64 nFees, bool fLastBlock);
 int64 GetProofOfStakeReward(int64 nCoinAge, int64 nFees, CBlockIndex* pindex);
 unsigned int ComputeMinWork(unsigned int nBase, int64 nTime);
